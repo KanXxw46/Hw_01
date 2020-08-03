be2853b5486a4fada96623e853b266fc5a74cefc
Андрей Кан <1kan_andreikkkk@mail.ru>
2020-08-03 14:30:36 +0600

    HomeWork

---
+++ Profile .cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hw_01
+{
+    public class Profile
+    {
+        public string Password { get; set; }
+        public string firstName { get; set; }
+        public string secondName { get; set; }
+        public int Number { get; set; }
+        public int Age { get; set; }
+        public string NickName { get; set; }
+        public string Сommunication { get; set; }
+        public int Data { get; set; }
+        public string Status { get; set; }
+        public string Calls { get; set; }
+
+    }
+
+    public class User
+    {
+        private string firstname;
+
+        public string Name
+        {
+            get
+            {
+                return firstname;
+            }
+
+            set
+            {
+                firstname = value;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Hw_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Profile> listOfUsers = new List<Profile>()
+        {
+        new Profile() { firstName = "John Doe",secondName = "Dactorov", Age = 42 },
+        };
+
+            for (int i = 0; i < listOfUsers.Count; i++)
+            {
+                Console.WriteLine(listOfUsers[i].firstName + " is " + listOfUsers[i].secondName + "is" + listOfUsers[i].Age + " years old");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
